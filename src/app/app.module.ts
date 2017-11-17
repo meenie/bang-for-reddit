bb186b600cdb1796b12a52d4b86cae6e9af65b18
Cody Lundquist <cody@getfeedback.com>
2017-11-16 16:55:25 -0800

    Big brother.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
-import { 
+import {
   StoreRouterConnectingModule,
-  RouterStateSerializer 
+  RouterStateSerializer
 } from '@ngrx/router-store'
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { Angulartics2Module } from 'angulartics2';
+import { Angulartics2GoogleAnalytics } from 'angulartics2/ga';
 
 import { CoreModule } from './core/core.module'
 import { AppComponent } from './core/containers/app.component';
@@ -27,6 +30,7 @@ import { environment } from '../environments/environment';
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),
     RouterModule.forRoot(routes, { useHash: true }),
+    Angulartics2Module.forRoot([Angulartics2GoogleAnalytics]),
     StoreRouterConnectingModule,
     !environment.production ? StoreDevtoolsModule.instrument() : [],
     CoreModule.forRoot()
