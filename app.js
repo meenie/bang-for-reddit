8953edc0adddf07b21c5e1e9b339007dd8b01fcd
Cody Lundquist <cody.lundquist@gmail.com>
2017-11-18 20:35:45 -0800

    Action redirect to ssl on heroku.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 const port = process.env.PORT || '3456';
 
-
+app.use(sslRedirect(['production'], 301));
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
