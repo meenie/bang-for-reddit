0f3c9f95f4cc65dcc1612724429aeea01d0f3422
Cody Lundquist <cody.lundquist@gmail.com>
2017-11-18 20:28:35 -0800

    Try to force SSL in prod.

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const forceSSL = require('express-force-ssl');
 
 const app = express();
 const port = process.env.PORT || '3456';
 
-
+app.use(forceSSL);
 app.use(express.static(path.join(__dirname, 'dist')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
