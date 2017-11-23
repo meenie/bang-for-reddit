32b01257fee2afb8192cb6ecebf6c01e7b946478
Cody Lundquist <cody@getfeedback.com>
2017-11-22 21:32:15 -0800

    Properly upgrade packages. Introduce version stuff.

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ const sslRedirect = require('heroku-ssl-redirect');
 
 const app = express();
 const port = process.env.PORT || '3456';
+const packageJson = require('./package.json');
 
 app.use(sslRedirect(['production'], 301));
 app.use(express.static(path.join(__dirname, 'dist')));
+app.get('/check-version', (req, res) => {
+  res.send(packageJson.version);
+});
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
